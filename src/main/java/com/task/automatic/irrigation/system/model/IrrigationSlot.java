800da19c53942174ab93ae3db87dccb5aaf971ee
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-03-24 04:19:21 +0200

    make the code ready for production

--- src/main/java/com/task/automatic/irrigation/system/model/IrrigationSlot.java
+++ src/main/java/com/task/automatic/irrigation/system/model/IrrigationSlot.java
@@ -1,6 +1,5 @@
 package com.task.automatic.irrigation.system.model;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,13 +17,10 @@ public class IrrigationSlot {
     @Id
     private String id;
 
-    @NotNull
     private LocalDateTime startTime;
 
-    @NotNull
     private LocalDateTime endTime;
 
-    @NotNull
     private double waterAmount;
 
     private SlotStatus status = SlotStatus.NOT_STARTED;
--- src/main/java/com/task/automatic/irrigation/system/model/Plot.java
+++ src/main/java/com/task/automatic/irrigation/system/model/Plot.java
@@ -1,9 +1,5 @@
 package com.task.automatic.irrigation.system.model;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,16 +15,11 @@ public class Plot {
     @Id
     private String id;
 
-    //note : the farm is a grid of plots so each location can be like this: "2,3"
-    @NotBlank
-    @Size(min = 3, max = 3)
-    @NotNull
+    //note : the farm is a grid of plots/cells so each location can be like this: "2,3"
     private String location;
 
-    @NotBlank
     private String soilType;
 
-    @NotBlank
     private String cropType;
 
     private PlotConfig config;
